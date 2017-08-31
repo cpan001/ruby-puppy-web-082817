da4787d90cb050d78a84fb92e47ec41911372dcb
cpan001 <github email address>
2017-08-30 21:00:17 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|dog_name| puts dog_name.name}
+  end
+
+end
+
+
+# binding.pry
+#
+# ""
